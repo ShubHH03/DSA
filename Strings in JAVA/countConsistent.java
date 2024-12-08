27cc2883a8b094c7d67a59e5c7f3c4ef57597d29
ShubHH03 <mak.shubham2003@gmail.com>
2024-12-08 19:43:17 +0530

    changes

---
+++ Strings in JAVA/countConsistent.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class countConsistent {
+    static int countConsistentStrings(String allowed, String[] words) {
+        Set<Character> set = new HashSet<>();
+        for(char ch: allowed.toCharArray()){
+            set.add(ch);
+        }
+
+        int count = 0;
+        for(String word: words){
+            boolean isConsistent = true;
+            for(int i=0;i<word.length();i++){
+                char ch = word.charAt(i);
+                if(!set.contains(ch)){
+                    isConsistent = false;
+                }
+            }
+            if(isConsistent){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        String allowed = "abc";
+        String[] words = {"a","b","c","ab","ac","bc","abc"};
+        int result = countConsistentStrings(allowed, words);
+        System.out.println(result);
+    }
+}
